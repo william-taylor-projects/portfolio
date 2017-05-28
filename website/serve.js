7a6ce2b09ce11f6e36752b5a98cbd17c458d7477
william-taylor <wi11berto@yahoo.co.uk>
2017-05-28 18:37:43 +0100

    minor improvements

--- website/serve.js
+++ website/serve.js
@@ -1,7 +1,9 @@
+const compress = require('compression');
 const express = require('express');
 const app = express();
 
 app.use(express.static('./'));
+app.use(compress());
 app.listen(3000, function () {
   console.log('Served on port 3000!')
 })
